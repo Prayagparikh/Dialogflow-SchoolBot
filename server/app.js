1645b3d333cde7993ecfd4296cb1affd32f7c74d
Prayag Parikh <prayagparikh1508@gmail.com>
2021-07-30 00:55:49 +0530

    server folder

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const path = require('path');
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
-const frontendDirPath = path.join(__dirname, "./UI");
+const frontendDirPath = path.join(__dirname, "../UI");
 app.use(express.static(frontendDirPath));
 
 // A unique identifier for the given session
